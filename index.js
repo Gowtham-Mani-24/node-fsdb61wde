43d6731c9ba6e924001196aca11127048450d621
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-09-22 12:49:54 +0530

    connected to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // import express module
 const express = require('express');
 
+//import mongoose module
+const mongoose = require('mongoose');
+
+//connect to mongodb database
+mongoose.connect(`mongodb+srv://workspaceofgowtham:8IACWDJR4efrmajR@cluster0.s3fqn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(() => {
+  console.log("connected to MongoDb successfully")
+})
+.catch((err)=> {
+  console.log("error connecting MongoDb database",err)
+});
+
 //create an express application
 const app = express();
 
